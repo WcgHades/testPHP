b8be9ee6ce675cba19d7fcfced28d3f027bbb022
WcgHades <2244813@qq.com>
2017-11-17 10:02:29 +0800

    第一天连续

--- test_01.php
+++ test_01.php
@@ -5,6 +5,8 @@
  * Date: 2017/11/17
  * Time: 9:29
  */
+header("Content-Type: text/html;charset=utf-8");
+define("MY_NAME",3.14159216);
+echo MY_NAME;
 
-echo "ceshi";
-echo "xiuga";
+echo '我的密码:'.MY_NAME;
