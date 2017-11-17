b6b54efc681c88436bb6ab7757a19524c8458a4c
WcgHades <2244813@qq.com>
2017-11-17 10:43:14 +0800

    新添加文件

---
+++ reg.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: WcgHades
+ * Date: 2017/11/17
+ * Time: 10:22
+ */
+header("Content-Type: text/html;charset=utf-8");
+$u = $_REQUEST['username'];
+echo $u.'<br />';
+
+$passwd = $_REQUEST['pwd'];
+echo $passwd.'<br />';
